affbf9d5f4e01cf35fea074e29a6d2fbcf037cbe
credicompany <credicompanycompany@gmail.com>
2026-03-29 06:47:45 -0500

    Update and rename service-worker.js to sw-v2.js

--- sw-v2.js
+++ sw-v2.js
@@ -1,2 +1,5 @@
 self.addEventListener("install", e => self.skipWaiting());
 self.addEventListener("activate", e => self.clients.claim());
+
+// SIN CACHE (evita errores)
+self.addEventListener("fetch", () => {});

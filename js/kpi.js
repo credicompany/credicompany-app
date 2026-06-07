c3e0229494f82db50327678da0fa0e93533a01cb
credicompany <credicompanycompany@gmail.com>
2026-06-07 13:42:53 -0500

    Update kpi.js

--- js/kpi.js
+++ js/kpi.js
@@ -33,19 +33,88 @@ wb.SheetNames[0]
 let json =
 XLSX.utils.sheet_to_json(hoja);
 
-let totalClientes =
-json.length;
+let totalClientes = json.length;
+
+let saldoCapital = 0;
+let clientesMora = 0;
+let cuotaMoraTotal = 0;
+
+json.forEach(c => {
+
+saldoCapital +=
+parseFloat(c["Saldo Capital"]) || 0;
+
+let atraso =
+parseFloat(c["Días Retraso"]) || 0;
+
+if(atraso > 0){
+clientesMora++;
+}
+
+cuotaMoraTotal +=
+parseFloat(c["Cuota Mora"]) || 0;
+
+});
 
 let resumen = `
 <div class="card">
-<h3>📊 Resumen KPI</h3>
 
-<p>👥 Total Clientes:
+<h3>📊 KPI Gerencial</h3>
+
+<p>👥 Clientes:
 <b>${totalClientes}</b></p>
+
+<p>💰 Saldo Capital:
+<b>S/ ${saldoCapital.toFixed(2)}</b></p>
+
+<p>🚨 Clientes en Mora:
+<b>${clientesMora}</b></p>
+
+<p>💵 Cuota Mora:
+<b>S/ ${cuotaMoraTotal.toFixed(2)}</b></p>
 
 </div>
 `;
+  let ranking = {};
 
+json.forEach(c => {
+
+let asesor =
+c["Asesor(a)"] || "Sin Asesor";
+
+let capital =
+parseFloat(c["Saldo Capital"]) || 0;
+
+if(!ranking[asesor]){
+ranking[asesor] = 0;
+}
+
+ranking[asesor] += capital;
+
+});
+
+let top = Object.entries(ranking)
+.sort((a,b)=>b[1]-a[1]);
+
+resumen += `
+<div class="card">
+
+<h3>🏆 Ranking Cartera</h3>
+`;
+
+top.forEach(r => {
+
+resumen += `
+<p>
+${r[0]}
+:
+S/ ${r[1].toFixed(2)}
+</p>
+`;
+
+});
+
+resumen += `</div>`;
 document.getElementById(
 "kpiResumen"
 ).innerHTML = resumen;

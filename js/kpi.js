9f0a6d5896a1702324fef73960a62876828bb193
credicompany <credicompanycompany@gmail.com>
2026-06-07 13:38:02 -0500

    Update kpi.js

--- js/kpi.js
+++ js/kpi.js
@@ -1,9 +1,59 @@
-//// =====================
+// =====================
 // KPI CREDICOMPANY
 // =====================
 
 function cargarExcelKPI(){
 
-    alert("✅ Módulo KPI operativo");
+let archivo =
+document.getElementById("excelKPI")
+.files[0];
+
+if(!archivo){
+
+alert("Seleccione un Excel");
+return;
+
+}
+
+let lector = new FileReader();
+
+lector.onload = function(e){
+
+let data =
+new Uint8Array(e.target.result);
+
+let wb =
+XLSX.read(data,{type:"array"});
+
+let hoja =
+wb.Sheets[
+wb.SheetNames[0]
+];
+
+let json =
+XLSX.utils.sheet_to_json(hoja);
+
+let totalClientes =
+json.length;
+
+let resumen = `
+<div class="card">
+<h3>📊 Resumen KPI</h3>
+
+<p>👥 Total Clientes:
+<b>${totalClientes}</b></p>
+
+</div>
+`;
+
+document.getElementById(
+"kpiResumen"
+).innerHTML = resumen;
+
+};
+
+lector.readAsArrayBuffer(
+archivo
+);
 
 }

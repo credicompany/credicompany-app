b4849c3fd6df121ff0f6fd6603f28798b3f360c1
credicompany <credicompanycompany@gmail.com>
2026-06-07 14:06:27 -0500

    Update kpi.js

--- js/kpi.js
+++ js/kpi.js
@@ -3,7 +3,44 @@
 // =====================
 
 function cargarExcelKPI(){
+function cargarMetasKPI(){
 
+let archivo =
+document.getElementById("excelMetas").files[0];
+
+if(!archivo){
+alert("Seleccione archivo de metas");
+return;
+}
+
+let lector = new FileReader();
+
+lector.onload = function(e){
+
+let data =
+new Uint8Array(e.target.result);
+
+let wb =
+XLSX.read(data,{type:"array"});
+
+let hoja =
+wb.Sheets[wb.SheetNames[0]];
+
+let json =
+XLSX.utils.sheet_to_json(hoja);
+
+localStorage.setItem(
+"metasKPI",
+JSON.stringify(json)
+);
+
+alert("✅ Metas cargadas correctamente");
+
+};
+
+lector.readAsArrayBuffer(archivo);
+
+}
 let archivo =
 document.getElementById("excelKPI")
 .files[0];

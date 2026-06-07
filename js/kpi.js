632c549f1a5c710de087b3f9428388da6c658d04
credicompany <credicompanycompany@gmail.com>
2026-06-07 13:17:13 -0500

    Update KPI module header and add alert function

--- js/kpi.js
+++ js/kpi.js
@@ -1,1 +1,9 @@
-// KPI Credicompany
+// ======================
+// KPI CREDICOMPANY
+// ======================
+
+function verKPIProduccion(){
+
+alert("Módulo KPI en construcción");
+
+}// KPI Credicompany

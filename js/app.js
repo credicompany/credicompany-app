38293026efa6b5828ac36a5379a734ad1e6ae5d7
credicompany <credicompanycompany@gmail.com>
2026-07-02 20:36:54 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
 console.log("✅ app.js cargado correctamente");
+// QR
+function abrirQR(el){
+imgQR.src=el.querySelector("img").src;
+modalQR.style.display="flex";
+}
+function cerrarQR(){modalQR.style.display="none";}
+function abrirTarifario(){
+
+document.getElementById("modalTarifario").style.display = "block";
+
+}
+
+function cerrarTarifario(){
+
+document.getElementById("modalTarifario").style.display = "none";
+
+}

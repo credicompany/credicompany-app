ea0dff0e60f7da7dc18bf506e1acd29d8eb18ade
credicompany <credicompanycompany@gmail.com>
2026-07-02 20:46:29 -0500

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,29 @@
+// ========================================
+// FIREBASE
+// Credicompany V2
+// ========================================
 
+console.log("🔥 firebase.js cargado");
+
+// Base de datos global
+window.db = firebase.database();
+
+// Funciones reutilizables
+
+function guardarFirebase(ruta, datos){
+
+    return db.ref(ruta).set(datos);
+
+}
+
+function leerFirebase(ruta){
+
+    return db.ref(ruta);
+
+}
+
+function pushFirebase(ruta, datos){
+
+    return db.ref(ruta).push(datos);
+
+}

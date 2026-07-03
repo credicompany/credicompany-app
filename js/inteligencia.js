5908b17670cd1d03bb9a793d5cce1cbd067f08a8
credicompany <credicompanycompany@gmail.com>
2026-07-02 20:05:01 -0500

    Update inteligencia.js

--- js/inteligencia.js
+++ js/inteligencia.js
@@ -1,25 +1,9 @@
-// =========================================
+// ======================================
 // CENTRO DE INTELIGENCIA
-// =========================================
+// ======================================
 
 function cargarCentroInteligencia(){
 
-    firebase.database()
-    .ref("resultadoMensual/actual")
-    .once("value")
-    .then((snapshot)=>{
-
-        if(!snapshot.exists()) return;
-
-        let datos = snapshot.val();
-
-        document.getElementById("intelIngreso").innerHTML =
-        "S/ " +
-        Number(datos.ingresos || 0)
-        .toLocaleString("es-PE",{
-            minimumFractionDigits:2
-        });
-
-    });
+    alert("✅ Inteligencia.js conectado correctamente");
 
 }

6a51a85bc81710137fab4ed64ca30413dee6eb19
credicompany <credicompanycompany@gmail.com>
2026-07-03 09:54:01 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -122,3 +122,30 @@ function iniciarSistema(){
     console.log("🚀 Iniciando CREDICOMPANY...");
 
 }
+// =====================================
+// RESET DIARIO
+// =====================================
+
+function iniciarResetDiario(){
+
+    let hoy = new Date().toLocaleDateString('en-CA');
+    let ultimo = localStorage.getItem("fechaSistema");
+
+    if(ultimo !== hoy){
+
+        let data = JSON.parse(localStorage.getItem("cartera")) || [];
+
+        data.forEach(c=>{
+            c.pagado_hoy = false;
+            c.pagado_monto = 0;
+        });
+
+        localStorage.setItem("cartera", JSON.stringify(data));
+        localStorage.setItem("bonosHoy", JSON.stringify({}));
+        localStorage.setItem("fechaSistema", hoy);
+
+        console.log("✅ Reset diario ejecutado");
+
+    }
+
+}

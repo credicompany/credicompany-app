85f81c37d55048a61f28d232e69ebd98dd1f394d
credicompany <credicompanycompany@gmail.com>
2026-07-03 09:40:59 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,23 @@
 
+// =====================================
+// CONFIGURACIÓN GLOBAL CREDICOMPANY
+// =====================================
+
+console.log("✅ config.js cargado");
+
+window.App = {
+
+    usuarios: [],
+    cartera: [],
+    historial: [],
+    metas: [],
+    produccion: [],
+
+    asesor: "",
+    esAdmin: false,
+
+    kpi: {},
+    resultadoMensual: {},
+    inteligencia: {}
+
+};
